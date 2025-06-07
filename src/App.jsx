842fe401fb3a28e04f81996e0abf88c866598d04
real-musafir <alshahadath.com>
2025-06-07 18:11:41 +0600

    cleaned few css issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
+import {
+  Calendar,
+  CreditCard,
+  MapPin,
+  Shield,
+  Trash2,
+  Truck,
+} from "lucide-react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <p className="text-3xl text-green-900">Welcome page</p>
-    </>
+    <div className=" w-full h-screen">
+      {/* header */}
+      <div className="flex">
+        <MapPin size={20} />
+        <Trash2 size={20} />
+        <Truck size={20} />
+        <Shield size={20} />
+        <Calendar size={20} />
+        <CreditCard size={20} />
+      </div>
+    </div>
   );
 }
 

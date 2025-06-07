f0effd813f95fe5f56bfec4164143dedb3ae25e1
real-musafir <alshahadath.com>
2025-06-07 19:52:32 +0600

    brougth header item title

--- src/App.jsx
+++ src/App.jsx
@@ -17,31 +17,37 @@ function App() {
       {/* header */}
       <div className="flex justify-between align-items-center mt-10">
         <MapPin className="text-blue-700" size={20} />
+        <p className="text-white text-lg">Postcode</p>
         <div
-          style={{ height: "3px", width: "6%" }}
+          style={{ height: "1px", width: "6%" }}
           className="my-auto bg-blue-700"
         />
         <Trash2 size={20} />
+        <p className="text-white text-lg">Waste Type</p>
         <div
-          style={{ height: "3px", width: "6%" }}
+          style={{ height: "1px", width: "6%" }}
           className=" my-auto bg-blue-700"
         />
         <Truck size={20} />
+        <p className="text-white text-lg">Select Skip</p>
         <div
-          style={{ height: "3px", width: "6%" }}
+          style={{ height: "1px", width: "6%" }}
           className=" my-auto bg-blue-700"
         />
         <Shield size={20} />
+        <p className="text-white text-lg">Permit Check</p>
         <div
-          style={{ height: "3px", width: "6%" }}
+          style={{ height: "1px", width: "6%" }}
           className=" my-auto bg-blue-700"
         />
         <Calendar size={20} />
+        <p className="text-white text-lg">Choose Date</p>
         <div
-          style={{ height: "3px", width: "6%" }}
+          style={{ height: "1px", width: "6%" }}
           className=" my-auto bg-blue-700"
         />
         <CreditCard size={20} />
+        <p className="text-white text-lg">Payment</p>
       </div>
     </div>
   );

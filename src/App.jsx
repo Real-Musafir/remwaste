c0e5f9ddcb9ae91d602b849d066040ea7db993db
real-musafir <alshahadath.com>
2025-06-07 23:40:23 +0600

    made page title subtitle

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,13 @@ function App() {
     <div className=" w-full h-screen px-4 justify-center">
       {/* header */}
       <ItemHeader currentStep={2} />
+
+      <p className="text-2xl text-white font-semibold mt-5">
+        Choose Your Skip Size
+      </p>
+      <p className="text-lg text-gray-400 mt-3">
+        Select the skip size that best suits your needs
+      </p>
     </div>
   );
 }
